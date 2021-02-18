8ad6d17d9c28148e56cfc479e55d9cf877132ef3
stabishali8 <stabishali8@gmail.com>
2021-02-18 12:47:00 +0500

    readme.md and .gitignore commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# node-login-signup-jwt-express-jwt-bezkoder
+Project based on Login Signup functionality with jwt in Node, Express, MySQL implementation follow the course bezkoder. 
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var corsOptions = {
     origin: "http://localhost:8081"
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -17,20 +17,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
-const Role = db.role;
 
-db.sequelize.sync({force: true}).then(() => {
-  console.log('Drop and Resync Db');
-//   initial();
-});
+// db.sequelize.sync({force: true}).then(() => {
+//   console.log('Drop and Resync Db');
+// //   initial();
+// });
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
+    res.json({ message: "Welcome to KOntrax application." });
 });
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
@@ -40,19 +39,3 @@ app.listen(PORT, () => {
 });
 // routes
 
-function initial() {
-    Role.create({
-        id: 1,
-        name: "user"
-    });
-
-    Role.create({
-        id: 2,
-        name: "moderator"
-    });
-
-    Role.create({
-        id: 3,
-        name: "admin"
-    });
-}
